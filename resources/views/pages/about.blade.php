d3b4a91566e7b1242c7f06f81571f1ff344dd47e
Muhammad Usama <usamaalee786@gmail.com>
2019-05-09 16:05:46 +0500

    first attempt

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -14,6 +14,8 @@
         </div><!-- /.container-fluid -->
     </div><!-- /.page-header -->
 
+
+
     <!-- ====== About Main Content ====== -->
     <section class="about-main-content">
         <div class="container">
